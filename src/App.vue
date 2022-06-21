3c5e4b5e624aa81b863fc913d0297de8deb00e87
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-21 19:46:45 +0530

    correct spelling

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="flex flex-col items-center justify-center h-screen">
-    <MultiStepForm :forms="forms" :steps="['Personal information 👶','Series 📺','Feeback 🌟']" 
+    <MultiStepForm
+      :forms="forms"
+      :steps="['Personal information 👶', 'Series 📺', 'Feedback 🌟']"
       :submit-action="submitAction"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
- import MultiStepForm from "./components/MultiStepForm.vue" 
- import Step1 from "./components/Step1.vue"
- import Step2 from "./components/Step2.vue"
- import Step3 from "./components/Step3.vue"
+import MultiStepForm from "./components/MultiStepForm.vue";
+import Step1 from "./components/Step1.vue";
+import Step2 from "./components/Step2.vue";
+import Step3 from "./components/Step3.vue";
 
- const forms = [Step1,Step2,Step3]
- const submitAction = () => {console.log("submitting form...")}
+const forms = [Step1, Step2, Step3];
+const submitAction = () => {
+  console.log("submitting form...");
+};
 </script>
 
 <style>
@@ -23,6 +27,6 @@
   -moz-osx-font-smoothing: grayscale;
 }
 body {
-    @apply bg-slate-800 text-white;
-  }
+  @apply bg-slate-800 text-white;
+}
 </style>
--- src/components/MultiStepForm.vue
+++ src/components/MultiStepForm.vue
@@ -1,39 +1,59 @@
 <template>
-    <div class="w-6/12">
-        <ul class="steps min-w-full">
-            <li v-for="(stepText,index) in props.steps" class="step" :class="index<=step ? 'step-primary' : ''">
-                {{stepText}}
-            </li>
-        </ul>
-       <div class="py-3"></div> 
-        <form @submit.prevent="formAction" class="min-w-full px-6"> 
-            <component
-                :is="props.forms[step]"
-            >
-            </component>
-       <div class="py-4"></div> 
-            <div class="flex justify-end">
-                <button class="btn btn-ghost" type="button" v-if="step!==0" @click="step--">Back</button>
-                <button class="btn btn-primary" type="submit" v-if="step!==props.steps.length-1">Next</button>
-                <button class="btn btn-primary" type="submit" v-if="step==props.steps.length-1">Submit</button>
-            </div>
-        </form>
-    </div>
+  <div class="w-6/12">
+    <ul class="steps min-w-full">
+      <li
+        v-for="(stepText, index) in props.steps"
+        class="step"
+        :class="index <= step ? 'step-primary' : ''"
+      >
+        {{ stepText }}
+      </li>
+    </ul>
+    <div class="py-3"></div>
+    <form @submit.prevent="formAction" class="min-w-full px-6">
+      <component :is="props.forms[step]"> </component>
+      <div class="py-4"></div>
+      <div class="flex justify-end">
+        <button
+          class="btn btn-ghost"
+          type="button"
+          v-if="step !== 0"
+          @click="step--"
+        >
+          Back
+        </button>
+        <button
+          class="btn btn-primary"
+          type="submit"
+          v-if="step !== props.steps.length - 1"
+        >
+          Next
+        </button>
+        <button
+          class="btn btn-primary"
+          type="submit"
+          v-if="step == props.steps.length - 1"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {ref} from "vue"
-import Step1 from "./Step1.vue"
+import { ref } from "vue";
+import Step1 from "./Step1.vue";
 
 let step = ref(0);
 const props = defineProps<{
-    forms:typeof Step1[],
-    steps:string[],
-    submitAction:()=>void
-}>()
+  forms: typeof Step1[];
+  steps: string[];
+  submitAction: () => void;
+}>();
 
 const formAction = () => {
-        if(step.value !== props.steps.length-1) return step.value++
-        props.submitAction()
-}
+  if (step.value !== props.steps.length - 1) return step.value++;
+  props.submitAction();
+};
 </script>
