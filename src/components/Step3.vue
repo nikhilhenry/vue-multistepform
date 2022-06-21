26f004560e24f19398e4ceadc44996396c99af77
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-21 12:32:46 +0530

    center and add text

--- src/components/Step3.vue
+++ src/components/Step3.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="form-control w-full">
+    <div class="form-control w-full flex items-center justify-center">
+    <h2 class="text-xl py-3">Rate this tutorial</h2>
         <div class="rating rating-lg">
           <input type="radio" name="rating-9" class="rating-hidden" />
           <input type="radio" name="rating-9" class="mask mask-star-2" />
